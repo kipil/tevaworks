33ef7e0a097dd2c668ddaf903979b3059164864e
kipil <63267474+kipil@users.noreply.github.com>
2020-10-16 05:56:34 -0500

    direccionamientos de barra a direcciones php

--- IndexEmpresa.php
+++ IndexEmpresa.php
@@ -42,9 +42,9 @@
       <div class="navbar">
         <ul clas="nav__ul">
           <li class="nav__li"><a href="#">Planes y facturas</a></li>
-          <li class="nav__li"><a href="busquedacv.html">Busquedas de CV'S</a></li>
-          <li class="nav__li"><a href="vacantes.html">Publicar vacantes</a></li>
-          <li class="nav__li"><a href="misvacantes.html">Mis vacantes</a></li>
+          <li class="nav__li"><a href="busquedacv.php">Busquedas de CV'S</a></li>
+          <li class="nav__li"><a href="vacantes.php">Publicar vacantes</a></li>
+          <li class="nav__li"><a href="misvacantes.php">Mis vacantes</a></li>
         </ul>
       </div>
 
@@ -55,7 +55,7 @@
             Mi Cuenta
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="IndexEmpresa.html">Mi Perfil</a>
+            <a class="dropdown-item" href="IndexEmpresa.php">Mi Perfil</a>
             <a class="dropdown-item" href="#">Cambiar Contraseña</a>
             <a class="dropdown-item" href="#">Cerrar Sesión</a>
           </div>
